2bac10321d82d055f32225f08113bf18c903a05b
bkjml <mattbirhane@gmail.com>
2024-06-11 10:34:55 +0300

    navbar and brand components

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -23,10 +23,11 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
-        <div className='gpt3__header-image'>
+      </div>
+      
+      <div className='gpt3__header-image'>
           <img src={ai} alt='ai'/>
         </div>
-      </div>
     </div>
   )
 }
