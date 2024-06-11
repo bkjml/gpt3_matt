75bf0ffea614f3a0c04d57a192da37bd026fec41
bkjml <mattbirhane@gmail.com>
2024-06-11 10:59:10 +0300

    features component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -8,8 +8,8 @@ const Feature = ({title, text}) => {
         <div />
         <h1>{title}</h1>
       </div>
-      <div className='gpt3__features-container__feature-text'>
-        {text}
+      <div className='gpt3__features-container_feature-text'>
+        <p>{text}</p>
       </div>
     </div>
   )
